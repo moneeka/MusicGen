fb29d0094dc363861a960079a1db4a224da9776e
Monica Mishra <monicamishra@college.harvard.edu>
2014-04-28 14:06:57 -0400

    fixing bugs in read

--- read.ml
+++ read.ml
@@ -3,13 +3,14 @@ open matrix
 
 let main_matrix = Matrix.music_matrix
 
-let increment_matrix_val (row:float) (col:float) : unit =
+let increment_matrix_val (row:int) (col:int) : unit =
     let n = main_matrix.(row).(col) in
     main_matrix.(row).(col) <- n +. 1.
 
 let rec note_counter (notes_list : int list) : float array array =
     match notes_list with
-    | empty -> raise (Failure "No notes recieved.")
+    | [] -> raise (Failure "No notes recieved.")
+    | [hd] -> main_matrix
     | hd1::hd2::tl -> increment_matrix_val hd1 hd2; note_counter (hd2::tl) 
 
 
