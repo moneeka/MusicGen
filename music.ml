69e2159416cfb71736fbbc86b1e3d3d8fc367624
Dipti Jasrasaria <djasrasaria@college.harvard.edu>
2014-04-28 11:39:11 -0400

    build song in music.ml

--- music.ml
+++ music.ml
@@ -1,4 +1,5 @@
 open Core.Std
+open matrix
 
 exception InvalidHex
 exception InvalidPitch
@@ -14,6 +15,9 @@ type obj = Note of pitch * float * int | Rest of float
 let p_to_int p =
   match p with | C -> 0 | Db -> 1 | D -> 2 | Eb -> 3 | E -> 4 | F -> 5
     | Gb -> 6 | G -> 7 | Ab -> 8 | A -> 9 | Bb -> 10 | B -> 11
+
+(*let int_to_t n =
+  match t with | 0 -> |...*)
 
 let int_to_p n =
   if (n < 0) || (n > 11) then raise InvalidPitch else
@@ -288,4 +292,28 @@ let next_note (current_prob : float array) (sum_of_rows : float) : float array =
         index:= !index + 1
         done ; next_note_helper !ans
 
+
+
+let rec build_song (note : float array) (song : float array list) : float array list =
+  let length = ref 50 in
+  while !length > 0 do
+  (* prob is a float array that contains the probabilities of the next note *)
+  let prob = vector_mult (*probability matrix*) note in
+  let new_note = next_note prob (sum_vector prob) in
+  length := !length - 1; song @ new_note
+  done 
+
+
+
+
+
+
+
+(* take in current note (a float array)
+generate next note (multiply note by matrix to generate probabilities, then use next_note)
+add current note to list
+substract length of note from thirty
+then run the function on the next note
+returns the list
+prompts for user input *)
 
